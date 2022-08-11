83b681fd9e256d2e317c6a168b93c9fa849a3e3b
Kasperone <kosecki.kasper@gmail.com>
2022-08-11 21:31:12 +0200

    NG-002 feat: created input component

---
+++ src/components/cInput.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="c-input">
+
+    <input type="text"
+         class="c-input__input"
+         :placeholder="placeholder">
+
+    <img src="../assets/icons/search-icon.svg"
+         alt='localization icon' class="c-input__icon">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'cInput',
+  props: {
+    placeholder: {
+      type: String,
+      default: 'Search Store',
+    },
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.c-input {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 2rem;
+
+  &__input {
+    position: relative;
+    width: 36.4rem;
+    height: 5.15rem;
+    padding-left: 4rem;
+    background: #F2F3F2;
+    border-radius: 1.5rem;
+    border: none;
+    resize: none;
+
+    &:focus {
+      outline: .1rem solid green;
+    }
+  }
+
+  &__icon {
+    position: absolute;
+    left: 2.5rem;
+  }
+}
+</style>
--- src/views/HomeScreenView.vue
+++ src/views/HomeScreenView.vue
@@ -1,14 +1,17 @@
 <template>
   <c-logo-and-localization />
+  <c-input />
 </template>
 
 <script>
 import cLogoAndLocalization from '../components/cLogoAndLocalization.vue'
+import cInput from '../components/cInput.vue'
 
 export default {
   name: 'HomeScreenView',
   components: {
     cLogoAndLocalization,
+    cInput
   }
 }
 
