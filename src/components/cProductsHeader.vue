c6ded454777e966b9d7fdd0e5ce9273ca4932f1a
Kasperone <kosecki.kasper@gmail.com>
2022-08-12 10:31:27 +0200

    NG-004 feat: working on heading component

---
+++ src/components/cProductsHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <c-heading text="Exclusive Offer"/>
+</template>
+
+<script>
+import cHeading from '../components/cHeading.vue';
+
+export default {
+  name: 'cProductsHeader',
+  components: {
+    cHeading
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeScreenView.vue
+++ src/views/HomeScreenView.vue
@@ -1,15 +1,17 @@
 <template>
   <c-logo-and-localization />
+  <c-products-header />
 </template>
 
 <script>
-import cLogoAndLocalization from '../components/cLogoAndLocalization.vue'
+import cLogoAndLocalization from '../components/cLogoAndLocalization.vue';
+import cProductsHeader from '../components/cProductsHeader.vue';
 
 export default {
   name: 'HomeScreenView',
   components: {
     cLogoAndLocalization,
+    cProductsHeader,
   }
 }
-
 </script>
