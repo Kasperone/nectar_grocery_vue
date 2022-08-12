371b26609257cc91f6eb9e92a498d199bdc181c7
Kasperone <kosecki.kasper@gmail.com>
2022-08-12 13:48:14 +0200

    NG-004 feat: created heading component

---
+++ src/components/cSeeAll.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-link class="c-see-all"
+               to="/blank">
+
+    <span class="c-see-all__text">See All</span>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'cSeeAll'
+}
+</script>
+
+<style scoped lang="scss">
+.c-see-all {
+  text-decoration: none;
+
+  &__text {
+    font-size: 1.6rem;
+    font-weight: 600;
+    font-style: normal;
+    color: #53B175;
+  }
+}
+</style>
